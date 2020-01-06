b1a364ac4a6224a7aafc2b9001632877066236a7
BENABDALLAH Yahya <yahya.ben.abdallah@gmail.com>
2020-01-06 20:43:09 +0100

    Question 1 & 2

---
+++ DS2017/marque.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS2017
+{
+    class marque
+    {
+        static public int count = 0;
+        public int ID;
+        public string libelle;
+
+        public marque()
+        {
+            count++;
+            this.ID = count;
+        }
+        public marque(string libelle)
+        {
+            count++;
+            this.ID = count;
+            this.libelle = libelle;
+        }
+    }
+}
---
+++ DS2017/vehicule.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS2017
+{
+    class vehicule
+    {
+        static public int count = 0;
+        public int ID;
+        public string matricule;
+        public int nbr_portes;
+        public string couleur;
+        public marque M;
+
+        public vehicule()
+        {
+            count++;
+            this.ID = count;
+        }
+        public vehicule(string matricule, int nbr_portes, string couleur, marque M)
+        {
+            count++;
+            this.ID = count;
+            this.matricule = matricule;
+            this.nbr_portes = nbr_portes;
+            this.couleur = couleur;
+            this.M = M;
+        }
+    }
+}
